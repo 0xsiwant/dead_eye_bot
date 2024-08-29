61db076a15833070ec2dde315839960f70323ef9
ameeryousufalibhatti <yousufbhatti2002@gmail.com>
2024-08-30 01:13:53 +0500

    vercel.json updated in backend!

--- backend/config/index.js
+++ backend/config/index.js
@@ -2,15 +2,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const PORT = process.env.PORT;
-const BOT_TOKEN = process.env.BOT_TOKEN;
-const WEB_LINK = process.env.WEB_LINK;
 const MONGODB_URI = process.env.MONGODB_URI;
 const FRONTEND_PATH = process.env.FRONTEND_PATH
 
 module.exports = {
     PORT,
-    BOT_TOKEN,
-    WEB_LINK,
     MONGODB_URI,
     FRONTEND_PATH
 }
