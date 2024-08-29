81e6a9120eb42afb23bff572caee618b8c68cf57
0xsiwant <129720275+0xsiwant@users.noreply.github.com>
2024-08-30 05:35:08 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
-const { MONGODB_URI } = require('./index');
 
 const connectDb = () => {
-    try{
+    try {
+        const MONGODB_URI = process.env.MONGODB_URI; // Ambil dari environment variables
+        
+        if (!MONGODB_URI) {
+            throw new Error('MONGODB_URI is not defined');
+        }
+        
         console.log("Connecting to db...");
-        mongoose.connect(MONGODB_URI);
+        mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("Database Connection Successful!");
-    }catch(error){
-        console.log("Database Connection failed!", error);
+    } catch (error) {
+        console.error("Database Connection failed!", error);
     }
 };
 
