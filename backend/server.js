f065fca8111d74126d5e45806dc3e429961f4d5f
ameeryousufalibhatti <yousufbhatti2002@gmail.com>
2024-08-30 00:59:30 +0500

    Bot seperated from server!

--- backend/server.js
+++ backend/server.js
@@ -1,55 +1,31 @@
 // Import required modules
-const { Telegraf } = require('telegraf');
 const express = require('express');
 const cors = require('cors');
-
 const connectDb = require('./config/db');
 const userRoutes = require('./Router/userRoutes');
-const { PORT, BOT_TOKEN, WEB_LINK, FRONTEND_PATH_VERCEL, FRONTEND_PATH_LOCAL } = require('./config/index');
+const { PORT, FRONTEND_PATH } = require('./config/index');
 
-const community_link = "https://t.me/yousufbhatti_1";
 
 const app = express();
 app.use(express.json());
 
 // Middleware
 app.use(cors({
-  origin: [FRONTEND_PATH_VERCEL, FRONTEND_PATH_LOCAL]
+  origin: FRONTEND_PATH
 }));
 
 // Database Connection
 connectDb();
 
-// Initialize the Telegram bot using the bot token
-const bot = new Telegraf(BOT_TOKEN);
-
-// Simple message handler for the bot
-bot.on('message', async (ctx) => {
-
-  await ctx.reply(`Hey, Welcome to Dead_Eye_Bot!`, {
-    reply_markup: {
-      inline_keyboard: [
-        [{ text: "👋 Start now!", web_app: { url: WEB_LINK } }],
-        [{ text: "Join our Community", url: community_link }],
-      ],
-    },
-  });
-});
-
-// Routes
-app.use('/', userRoutes);
 
 // Start express server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+// Routes
+app.use('/', userRoutes);
 
 // Test API
 app.get('/', (req, res) => {
-  res.send('Bot is running');
+  res.send('Backend is running');
 });
-
-// Start the bot
-bot.launch()
-  .then(() => console.log('Telegram bot is running'))
-  .catch((err) => console.error('Failed to launch bot:', err));
---
+++ bot/server.js
@@ -0,0 +1,39 @@
+const { Telegraf } = require('telegraf');
+const express = require('express');
+const { PORT, BOT_TOKEN, WEB_LINK } = require('./config/dotenv');
+const community_link = "https://t.me/yousufbhatti_1";
+
+const app = express();
+
+app.use(express.json());
+
+// Initialize the Telegram bot using the bot token
+const bot = new Telegraf(BOT_TOKEN);
+
+// Simple message handler for the bot
+bot.on('message', async (ctx) => {
+
+    await ctx.reply(`Hey, Welcome to Dead_Eye_Bot!`, {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: "👋 Start now!", web_app: { url: WEB_LINK } }],
+                [{ text: "Join our Community", url: community_link }],
+            ],
+        },
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Bot is running on port: ${PORT}`);
+})
+
+app.get('/', (req, res) => {
+    res.send("Bot running succesfuly!");
+})
+
+
+// Start the bot
+bot.launch()
+  .then(() => console.log('Telegram bot is running'))
+  .catch((err) => console.error('Failed to launch bot:', err));
